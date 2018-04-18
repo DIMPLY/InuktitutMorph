e9d8ac168a9800f957652b22d059201e36b53cc9
Jakob Prange <jp1724@georgetown.edu>
2018-04-18 14:21:09 -0400

    modified preprocessing

--- preprocess.py
+++ preprocess.py
@@ -6,9 +6,13 @@ with open(sys.argv[1]) as f:
         word, morph = line.split("\t")
         chars = " ".join(word)
         morphs = morph.split(", ")
+        _morphs = []
         for m in morphs:
             segments = []
             for segment in m.split():
                 segment = segment.split("_")[-1]
+                segment = segment.replace("+", "")
+                segment = segment.replace("-", " ")
                 segments.append(segment)
-            print(chars + "\t" + " ".join(segments))
+            _morphs.append(" ".join(segments))
+        print(chars + "\t" + " + ".join(_morphs))
