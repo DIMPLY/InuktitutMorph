45d4e6c812eac85005664f97762efac347d0dbe3
Jakob Prange <jp1724@georgetown.edu>
2018-04-23 14:14:54 -0400

    added current version of wiktionary preprocessing

--- wiktionary2morpho.py
+++ wiktionary2morpho.py
@@ -3,18 +3,21 @@ import sys
 
 forms2analyses = {}
 
-with open(sys.argv[1]) as f:
+filename = sys.argv[1]
+pos = "NOUN" if "noun" in filename else "VERB" if "verb" in filename else ""
+
+with open(filename) as f:
     for line in f:
         line = line.strip()
         form, lemma, analysis = line.split(",")
         if form not in forms2analyses:
             forms2analyses[form] = []
-        forms2analyses[form].append(" ".join(lemma) + " " + " ".join(analysis.split(":")))
-#        result = " ".join(form.lower()) + "\t"  # + " ".join(lemma)
-#        for feature in analysis.split(":"):
-#            k, v = feature.split("=")
-#            result += v.upper() + " "
-#        print(result.strip())
+        forms2analyses[form].append(pos + " " + " ".join(analysis.split(":")))
+        #result = " ".join(form.lower()) + "\t" + pos # + " ".join(lemma)
+        #for feature in analysis.split(":"):
+        #    k, v = feature.split("=")
+        #    result += v.upper() + " "
+        #print(result.strip())
 
 
 for form, _as in forms2analyses.items():
